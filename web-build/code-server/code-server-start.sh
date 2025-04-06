5123b2cf72828ed8ee3a98f4d6b8b2e94c3d1ce9
Mario Bortolotti <bortomar@email.cz>
2025-04-07 01:40:27 +0200

    code-server data dir optional

--- web-build/code-server/code-server-start.sh
+++ web-build/code-server/code-server-start.sh
@@ -3,6 +3,11 @@ pids=$(pgrep -f "/usr/lib/code-server")
 if [ -z "$pids" ]; then
 	echo "Starting code-server..."
 	setsid sh -c 'code-server --bind-addr=0.0.0.0:8080 --auth=none /var/www/html > ~/code-server.log 2>&1' &
+	if [ ! -d ~/.local/share/code-server ]; then
+	# TODO: install extensions
+	    code-server --install-extension eamodio.gitlens
+	    code-server --install-extension devsense.phptools-vscode
+	fi
 else
 	echo "Already running!"
 fi
